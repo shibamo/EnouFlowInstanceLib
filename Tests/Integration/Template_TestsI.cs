846004b98a2e8ae935a56faec1ade22e1e2028ae
shibamo <qinchao@hotmail.com>
2017-06-16 23:58:17 -0700

    Refactor: import testing frameworks and utils

---
+++ Tests/Integration/Template_TestsI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using EnouFlowTemplateLib.Tests.Attributes;
+using NSubstitute;
+
+using EnouFlowOrgMgmtLib;
+using EnouFlowTemplateLib;
+
+namespace EnouFlowInstanceLib.Tests.Integration
+{
+  [TestFixture]
+  public class Template_TestsI
+  {
+    private EnouFlowInstanceContext db = null;
+
+    [SetUp]
+    public void Setup()
+    {
+      db = new EnouFlowInstanceContext();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      db.Dispose();
+    }
+  }
+}
